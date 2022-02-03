906b5b31e8ede871feb28cf5c3233d034542c9b8
heesu <sw6297@gmail.com>
2022-02-03 20:20:24 +0900

    DRF 웹 API - JSON 응답뷰

---
+++ django_rest/instagram/views.py
@@ -0,0 +1,23 @@
+from rest_framework.viewsets import ModelViewSet
+from .serializers import PostSerializer
+from .models import Post
+
+class PostViewSet(ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+    # def dispatch(self, request, *args, **kwargs):
+    #     print("request.body :", request.body)  # print 비추천, logger 추천
+    #     print("request.POST :", request.POST)
+    #     return super().dispatch(request, *args, **kwargs)
+
+
+
+# def post_list(request):
+#     # 2개 분기
+#     pass
+#
+#
+# def post_detail(request, pk):
+#     # request.method # => 3개 분기
+#     pass
