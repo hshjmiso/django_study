200f9a50f15f89d34ee9225946896a1a80da8d99
heesu <sw6297@gmail.com>
2022-02-08 14:47:17 +0900

    DRF 웹 API - JSON 직렬화

--- django_rest/instagram/serializers.py
+++ django_rest/instagram/serializers.py
@@ -1,8 +1,23 @@
-from rest_framework.serializers import ModelSerializer
+from django.contrib.auth import get_user_model
+from rest_framework import serializers
 from .models import Post
 
+class AuthorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = get_user_model()
+        fields = ['username', 'email '   ]
 
-class PostSerializer(ModelSerializer):
+class PostSerializer(serializers.ModelSerializer):
+    # username = serializers.ReadOnlyField(source='author.username')
+    # author_email = serializers.ReadOnlyField(source='author.email')
+    author = AuthorSerializer()
+
     class Meta:
         model = Post
-        fields = '__all__'
+        fields = [
+            'pk',
+            'author',
+            'message',
+            'created_at',
+            'updated_at',
+        ]
