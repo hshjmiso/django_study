5d81a06ed766af3db3976a37b5e1103356a7dd0e
heesu <sw6297@gmail.com>
2022-02-10 15:34:07 +0900

    Posting List API - Django REST framework - DEFAULT_PERMISSION_CLASSES

--- django-with-react/backend/backend/settings/common.py
+++ django-with-react/backend/backend/settings/common.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # third apps
+    'rest_framework',
     # local apps
     'instagram',
     'accounts'
@@ -130,3 +131,9 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ]
+}
--- django-with-react/backend/instagram/admin.py
+++ django-with-react/backend/instagram/admin.py
@@ -5,11 +5,12 @@ from .models import Post, Comment, Tag
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-    list_display = ["photo_tag", "caption"]
-    list_display_links = ["caption"]
+    pass
+    # list_display = ["photo_tag", "caption"]
+    # list_display_links = ["caption"]
 
-    def photo_tag(self, post):
-        return mark_safe(f"<img src={post.photo.url} style='width: 100px;' />")
+    # def photo_tag(self, post):
+    #     return mark_safe(f"<img src={post.photo.url} style='width: 100px;' />")
 
 
 @admin.register(Comment)
